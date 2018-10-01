11a35650ac7f6c78cdec20892885d2845e5bbf5c
Mats Jorgensen <mats.jorgensen@brreg.no>
2018-10-01 22:50:38 +0200

    Implement gameloop and basic IO

--- src/index.js
+++ src/index.js
@@ -1,91 +1,168 @@
 import './index.scss';
 
-const marioRef = document.querySelector('#mario');
-const luigiRef = document.querySelector('#luigi');
+const FACTOR_MOVEMENT_SPEED = 10;
+const FACTOR_FALL_SPEED = 15;
+const FACTOR_JUMP_SPEED = 50;
 
+const keyState = {};
 const marioState = {
-  jumpCount: 0,
-  position: {
-    x: 0,
-    y: 0
+  ref: document.querySelector('#mario'),
+  jump: {
+    count: 0,
+    forceFall: false
   }
 };
 
 const luigiState = {
-  jumpCount: 0,
-  position: {
-    x: 0,
-    y: 0
+  ref: document.querySelector('#luigi'),
+  jump: {
+    count: 0,
+    forceFall: false
   }
 };
 
 (function init() {
+  initializePositions();
   registerInput();
+  startGameloop();
 })();
 
-function registerInput() {
-  document.addEventListener('keyup', (e) => {
-    console.log(e.code);
+function initializePositions() {
+  marioState.ref.style.left = '0px';
+  marioState.ref.style.bottom = '500px';
 
-    switch (e.code) {
-      case 'KeyW':
-        jump(marioRef, marioState, 'right');
-        break;
-      case 'ArrowUp':
-        jump(luigiRef, luigiState, 'left');
-        break;
+  luigiState.ref.style.left = '800px';
+  luigiState.ref.style.bottom = '100px';
+  luigiState.ref.style.transform = 'rotateY(180deg)';
+}
+
+function registerInput() {
+
+  document.addEventListener('keydown', e => {
+    if (e.repeat) {
+      return;
     }
+
+    keyState[ e.code ] = true;
+  });
+
+  document.addEventListener('keyup', e => {
+    keyState[ e.code ] = false;
   });
 }
 
-function rotate(ref, state, facing) {
-  ref.animate([
+function rotateZ(state) {
+  const currentTransform = state.ref.style.transform.replace(/rotateZ\(.*\)/, '');
+
+  state.ref.animate([
     {
-      transform: facing === 'right' ? 'rotateZ(0deg)' : 'rotateY(180deg) rotateZ(0deg)'
+      transform: `${currentTransform} rotateZ(0deg)`
     },
     {
-      transform: facing === 'right' ? 'rotateZ(360deg)' : 'rotateY(180deg) rotateZ(360deg)'
+      transform: `${currentTransform} rotateZ(360deg)`
     },
   ], {
     duration: 400,
     iterations: 1
-  })
-}
-
-function move(ref) {
-  ref.animate([
-    {
-      left: '30px'
-    }, {
-      left: '1300px'
-    }
-  ], {
-    duration: 5000,
-    delay: 2000
   });
 }
 
-function jump(ref, state, facing) {
+function move(ref, direction) {
+  switch (direction) {
+    case 'right':
+      ref.style.left = `${parseInt(ref.style.left) + 1 * FACTOR_MOVEMENT_SPEED}px`;
+      break;
+    case 'left':
+      ref.style.left = `${parseInt(ref.style.left) - 1 * FACTOR_MOVEMENT_SPEED}px`;
+      break;
+  }
+}
 
-  state.jumpCount++;
-
-  if (state.jumpCount === 3) {
-    rotate(ref, state, facing);
-    state.jumpCount = 0;
+function jump(state) {
+  if (!isInAir(state.ref)) {
+    state.jump.count++;
   }
 
-  ref.animate([
-    {
-      bottom: '0px'
-    },
-    {
-      bottom: `${100 + Math.random() * 300}px`
-    },
-    {
-      bottom: '0px'
-    }
-  ], {
-    duration: 500,
-    iterations: 1
-  });
+  if (reachedMaxJumpingHeight(state.ref)) {
+    state.jump.forceFall = true;
+  }
+
+  state.ref.style.bottom = `${parseInt(state.ref.style.bottom) + 1 * FACTOR_JUMP_SPEED}px`;
+
+  if (state.jump.count === 3) {
+    rotateZ(state, 'right');
+    state.jump.count = 0;
+  }
+}
+
+function processMarioKeys() {
+  if (keyState[ 'KeyD' ]) {
+    move(marioState.ref, 'right');
+  }
+
+  if (keyState['KeyA']) {
+    move(marioState.ref, 'left');
+  }
+
+  if (keyState['KeyW'] && !marioState.jump.forceFall) {
+    jump(marioState);
+  } else if (isInAir(marioState.ref)) {
+    marioState.jump.forceFall = true;
+  }
+}
+
+function processLuigiKeys() {
+  if (keyState[ 'ArrowLeft' ]) {
+    move(luigiState.ref, 'left');
+  }
+
+  if (keyState[ 'ArrowRight' ]) {
+    move(luigiState.ref, 'right');
+  }
+
+  if (keyState['ArrowUp'] && !luigiState.jump.forceFall) {
+    jump(luigiState);
+  } else if (isInAir(luigiState.ref)) {
+    luigiState.jump.forceFall = true;
+  }
+}
+
+function processKeys() {
+  processMarioKeys();
+  processLuigiKeys();
+}
+
+function isInAir(ref) {
+  return parseInt(ref.style.bottom) > 0;
+}
+
+function reachedMaxJumpingHeight(ref) {
+  return parseInt(ref.style.bottom) >= 500;
+}
+
+function calculateFall(ref) {
+  const y = parseInt(ref.style.bottom);
+
+  return `${Math.max(y - 1 * FACTOR_FALL_SPEED, 0)}px`
+}
+
+function processGravity() {
+  if (isInAir(marioState.ref)) {
+    marioState.ref.style.bottom = calculateFall(marioState.ref);
+  } else {
+    marioState.jump.forceFall = false;
+  }
+
+  if (isInAir(luigiState.ref)) {
+    luigiState.ref.style.bottom = calculateFall(luigiState.ref);
+  } else {
+    luigiState.jump.forceFall = false;
+  }
+}
+
+function startGameloop() {
+  setInterval(() => {
+    processKeys();
+    processGravity();
+  }, 10);
 }
